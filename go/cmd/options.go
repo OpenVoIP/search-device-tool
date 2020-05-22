5427ba2708f2d6aef967a87ef05e981792dba66f
tqcenglish <tqcenglish@gmail.com>
2020-05-22 11:31:14 +0800

    调小窗口

--- go/cmd/options.go
+++ go/cmd/options.go
@@ -7,7 +7,7 @@ import (
 )
 
 var options = []flutter.Option{
-	flutter.WindowInitialDimensions(800, 1280),
+	flutter.WindowInitialDimensions(800, 600),
 	flutter.WindowDimensionLimits(800, 600, 1900, 1200),
 	flutter.AddPlugin(&scan.PluginInfo{}),
 }
